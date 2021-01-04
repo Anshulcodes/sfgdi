4174e1adad9c78ef5867d01e8b2a720314e757b3
Anshul <nshulsingh3@gmail.com>
2021-01-05 00:17:34 +0530

    Application Context explanation- Controller

--- src/main/java/guru/springframework/sfgdi/SfgdiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgdiApplication.java
@@ -2,12 +2,17 @@ package guru.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgdiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgdiApplication.class, args);
+		 ApplicationContext ac=  SpringApplication.run(SfgdiApplication.class, args);
+		 MyController mc=(MyController) ac.getBean("myController");
+		 System.out.println(mc.sayHello());
 	}
 
 }
